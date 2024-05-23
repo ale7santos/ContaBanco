de23920e0ac4bf7c589e81824615a3d117b22c1d
Alessandro Santos <87937813+ale7santos@users.noreply.github.com>
2024-05-23 18:13:21 -0300

    Update ContaTerminal.java

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -4,23 +4,27 @@ import java.util.Scanner;
 public class ContaTerminal {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		String usuario = " ";
 		int conta = 0;
 		String agencia = " ";
 		String nomeCliente = " ";
-		Double saldo = 0.0;
+		Double saldo = 237.48;
+		
+		System.out.println("Por favor, digite o seu usário: ");
+		usuario = sc.next();
 
-		System.out.println("Por favor, digite o número da Agência !");
-		agencia = sc.nextLine();
+		System.out.println("Por favor, digite o número da Agência: ");
+		agencia = sc.next();
 		
 		System.out.println("Por favor, digite o número da sua Conta: ");
 		conta = sc.nextInt();
 		
 		System.out.println("Por favor, digite o seu nome completo: ");
-		nomeCliente = sc.nextLine();
+		nomeCliente = sc.next();
 		
-		System.out.println("Usuário: " + agencia);
-		System.out.println("Olá, " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + 
-							agencia + ", conta " + " e seu saldo "+ saldo + " já está disponível para saque.");
+		System.out.println("Usuário: " + usuario);
+		System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco! Sua agência é " + 
+							agencia + ", conta " + conta + " e seu saldo de R$"+ saldo + " já está disponível para saque.");
 		sc.close();
 	}
 }
